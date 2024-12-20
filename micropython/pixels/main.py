39e654f65228f1a1b3ffedf73fa4ec66bcd3148f
Tom Brady <Tom.Brady@analog.com>
2024-12-20 14:19:16 -0500

    moved to main.py

--- micropython/pixels/main.py
+++ micropython/pixels/main.py
@@ -4,12 +4,16 @@ import array, time
 import math
 import random
 from machine import Pin
-from pixels import BATTERY
+from pixels import PIXELS
 
 def main():
-    b = BATTERY(0, 8, 0.1)
+    p = PIXELS(0, 64, 0.1)
+
     while(1):
-        b.life_meter()
+        for i in range(360):
+            p.fill_pixels_array([p.angle_to_rgb(i)] * p.num_leds)
+            p.show_pixels()
+
 
 
 if __name__ == "__main__":
